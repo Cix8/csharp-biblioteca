d230316beebb07d31524d6e4228719782b479618
Cix8 <francesco.natal_fn@libero.it>
2022-09-20 14:51:10 +0200

    add Library_class

---
+++ Library.cs
@@ -0,0 +1,32 @@
+﻿public class Library
+{
+    private string name;
+    private List<Book> books;
+    private List<Dvd> dvds;
+
+    public Library(string name)
+    {
+        this.name = name;
+    }
+
+    public List<Book> Books
+    {
+        get
+        {
+            return books;
+        } set
+        {
+            books = value;
+        }
+    }
+
+    public List<Dvd> Dvds
+    {
+        get { 
+            return dvds; 
+        } set
+        {
+            dvds = value;
+        }
+    }
+}
