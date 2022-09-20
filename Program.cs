670e96a74d544cf0f88992b057696c14ffc6d0a7
Cix8 <francesco.natal_fn@libero.it>
2022-09-20 14:12:07 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//cognome,
+//nome,
+//email,
+//password,
+//recapito telefonico
+
+
+
+public class User
+{
+    private string surname;
+    private string name;
+    private string email;
+    private string password;
+    private string phone;
+
+    public User(string surname, string name, string email, string password, string phone)
+    {
+        this.surname = surname;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+}
